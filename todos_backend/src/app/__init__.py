13c27d46fbe5b789075a6e9186fe65b15167d58a
Devansh Kumar <devansh3790@outlook.com>
2020-05-26 15:20:02 +0530

    DATABASE_URI override issue resolved

--- todos_backend/src/app/__init__.py
+++ todos_backend/src/app/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('TODOS_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
     # jwt extention config
